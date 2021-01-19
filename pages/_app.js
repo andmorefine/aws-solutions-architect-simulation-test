fab1792ede7a57fe91a98e125cac6facb8ea8275
tanaka.satoshi <tanaka.satoshi@a-tm.co.jp>
2021-01-20 01:27:35 +0900

    fix checkbox & custom.scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '../styles/custom.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/simulation_test_1/[id].js
+++ pages/simulation_test_1/[id].js
@@ -4,34 +4,69 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Layout from '../../components/layout'
 import contents from '../../contents/simulation_test_1.json'
+import { Button, Modal } from 'react-bootstrap';
 
 const QuestionDetail = ({ question, answerOptions }) => {
   const router = useRouter()
 
-  const state = {
+  // modal
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const initialState = {
     answer: '',
     correct: false,
     message: '',
+    checkedList: [],
   }
-  const [answer, setAnswer] = useState(state)
+  const [answer, setAnswer] = useState(initialState)
 
   const answerText = () => {
     return answer.correct ? "よくできました!" : "不正解です。もう一度確かめてください。"
   }
+  const noteText = () => {
+    return (question.type === 'radio') ? "選択項目を選択してください" : `${question.num_checkboxions}つ選択してください`
+  }
+
+  const unCheck = () => {
+    for (const element of document.getElementsByName(question.type)) {
+      element.checked = false;
+    }
+  }
 
   const handleChange = e => {
-    setAnswer({...answer, answer: e.target.value.toString()})
+    if (question.type === 'radio') {
+      setAnswer({ ...answer, answer: e.target.value.toString() })
+    } else {
+      const element = document.getElementsByName(question.type)
+      const checked = Array.prototype.filter.call(element, value => value.checked)
+      const checkedList = checked.map(item => item.value)
+      setAnswer({ ...answer, checkedList })
+    }
   }
 
   const answerCheck = () => {
-    const content = answerOptions.find((value) => (value.option_id == answer.answer))
-    if (!content) return false
+    let correct, message
+    let content = answerOptions.find((value) => (value.option_id == answer.answer))
+    if (question.type === 'checkbox') {
+      content = answerOptions.filter((value) => value.correct === true)
+      const answerArray = content.map((value) => value.option_id.toString())
+      content.correct = (answer.checkedList.toString() === answerArray.toString()) ? true : false
+      const messageArray = answerOptions.filter((value) => answer.checkedList.includes(value.option_id.toString()))
+      const messageString = messageArray.map((value) => value.message)
+      content.message = messageString.join("\n")
+    }
+    if (!answer.answer && answer.checkedList.length === 0) {
+      return handleShow()
+    }
+    if (question.type === 'checkbox' && answer.checkedList.length != question.num_checkboxions) {
+      return handleShow()
+    }
+    correct = content.correct
+    message = content.message
 
-    setAnswer({
-      ...answer,
-      correct: content.correct,
-      message: content.message,
-    })
+    setAnswer({ ...answer, correct, message })
   }
 
   const handleClick = e => {
@@ -40,11 +75,8 @@ const QuestionDetail = ({ question, answerOptions }) => {
   }
 
   const nextLink = () => {
-    setAnswer({...answer,
-      answer: '',
-      correct: false,
-      message: '',
-    })
+    unCheck()
+    setAnswer(initialState)
     router.push(`/simulation_test_1/${question.id + 1}`)
   }
 
@@ -53,10 +85,10 @@ const QuestionDetail = ({ question, answerOptions }) => {
       <h1>問題{question.id}</h1>
       <p>{question.question_text}</p>
       {answerOptions.map((answer) => (
-        <div className="form-check" key={answer.option_id}>
-          <input className="form-check-input" type='radio' name={`${question.type}${answer.question_id}`} value={answer.option_id} id={`flex${answer.option_id}`} onChange={handleChange} />
-          <label className="form-check-label" htmlFor={`flex${answer.option_id}`}>
-            {answer.answer_text}
+        <div className="form-check py-1" key={answer.option_id}>
+          <input className="form-check-input" type={question.type} name={question.type} value={answer.option_id} id={`flex${answer.option_id}`} onChange={handleChange} />
+          <label className="form-check-label w-100" htmlFor={`flex${answer.option_id}`}>
+            {answer.option_id}. {answer.answer_text}
           </label>
         </div>
       ))}
@@ -68,10 +100,19 @@ const QuestionDetail = ({ question, answerOptions }) => {
       </>) : (<></>)}
       <div className="text-center my-3">
         <button type="button" className="btn btn-success mx-2" onClick={handleClick}>解答を確認する</button>
-        {answer.correct ? (<>
-          <button type="button" className="btn btn-primary mx-2" onClick={nextLink}>次へ</button>
-        </>) : (<></>)}
+        <button type="button" className="btn btn-primary mx-2" onClick={nextLink}>次へ</button>
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title><i className="bi bi-exclamation-triangle-fill me-2 text-warning"></i>注意</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{noteText()}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            <i className="bi bi-x-square"></i>
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Layout>
   )
 }
